adc07e36e8796bf1f4e8276f121049f3ae3440e6
VitaBabenko <babenkovita1988@gmail.com>
2023-05-12 20:26:36 +0300

    done

---
+++ routes/api/contacts-router.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const contactsController = require("../../controllers/contacts-controller");
+
+const schemas = require("../../schemas/contacts-schemas");
+
+const { validateBody } = require("../../decorators");
+
+const router = express.Router();
+
+router.get("/", contactsController.getAllContacts);
+
+router.get("/:id", contactsController.getContactById);
+
+router.post(
+  "/",
+  validateBody(schemas.contactAddSchema),
+  contactsController.addContact
+);
+
+router.delete("/:id", contactsController.deleteContactById);
+
+router.put(
+  "/:id",
+  validateBody(schemas.contactAddSchema),
+  contactsController.updateContactById
+);
+
+module.exports = router;
